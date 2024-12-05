69b05b165c4ea466e2e98c3bc4b37ad3b1a9a430
ERIKA BIBIANO <al222010568@gmail.com>
2024-12-05 13:43:33 -0600

    Nuevo mensaje para el commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { interval,map, filter } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rxjs';
+  ngOnInit():void {
+    const observable = interval(1000);
+    const suscription = observable.pipe( 
+      map(val => val * val), filter(resp => resp % 2 !== 0) 
+    ).subscribe({
+   next:(resp) => console.log(resp),
+    });
+    setInterval(() => {
+      suscription.unsubscribe();
+    }, 1000);
+  }
 }
